cb1b0a46cb6067e39efebd0f16d7911b5092f90d
Leonid <jiehuh87@yandex.ru>
2021-10-12 19:37:28 +0300

    Ex1,2

---
+++ GU_ASP.NET_MVC_Core/GU_ASP.NET_MVC_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GU_ASP.NET_MVC_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
