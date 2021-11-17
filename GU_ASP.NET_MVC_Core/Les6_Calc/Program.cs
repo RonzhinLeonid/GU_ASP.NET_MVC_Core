d85aab2c25849c67476a46a07540b9ed8d48783a
Leonid <jiehuh87@yandex.ru>
2021-11-17 22:35:53 +0300

    upd

--- GU_ASP.NET_MVC_Core/Les6_Calc/Program.cs
+++ GU_ASP.NET_MVC_Core/Les6_Calc/Program.cs
@@ -10,10 +10,13 @@ namespace Les6_Calc
         {
             var builder = new ContainerBuilder();
 
+            //Геннадий, тут аналогичный вопрос как и в первом задании? Если регистрирую все действия, то как потом вызывать нужное?
+
             //builder.RegisterType<Division>().As<IAction>().SingleInstance();
             builder.RegisterType<Multiplication>().As<IAction>().SingleInstance();
             //builder.RegisterType<Subtraction>().As<IAction>().SingleInstance();
             //builder.RegisterType<Sum>().As<IAction>().SingleInstance();
+
             builder.RegisterType<Calc>().As<ICalc>().SingleInstance();
 
             IContainer container = builder.Build();
